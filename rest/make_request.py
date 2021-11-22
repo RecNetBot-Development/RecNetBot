e020153638c8593e38bfb90700515f46b5245de0
Kian <86496692+longwalter@users.noreply.github.com>
2021-11-22 19:41:01 +0000

    improved response format

--- rest/make_request.py
+++ rest/make_request.py
@@ -1,7 +1,11 @@
 import asyncio
+from os import stat
 import aiohttp
+from aiohttp.helpers import strip_auth_from_url
+from attr import dataclass
 
 from rest import helpers
+from rest.wrapper.exceptions import AccountNotFound
 
 class APIRequest:
     def __init__(self, path, method, params=None, data=None):
@@ -18,8 +22,16 @@ class APIRequest:
 
     async def make_request(self):
         async with aiohttp.request(method=self._method, url=self._path, params=self._params, data=self._data) as response:
-            if response.ok:
-                data = await response.json()
-                return data
-            else:
-                return {}
+                body = await response.json()
+                status_code = response.status
+                success = response.ok
+        
+        @dataclass
+        class Response:
+            status: int
+            success: bool
+            data: int | dict | str
+
+        return Response(status_code, success, body)
+
+
