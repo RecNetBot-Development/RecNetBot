1064ad0501681d8a032da7d59f3503c4f5c26a09
Colin Sullivan <colinsullivan03@gmail.com>
2022-02-08 15:33:09 -0600

    Add support for events.

--- rec_net/managers/event/event.py
+++ rec_net/managers/event/event.py
@@ -1,11 +1,12 @@
 from attr import dataclass, field
-from .event_options import EventOptions
+from ...helpers import date_to_unix
 from ..base import BaseDataclass
 
 @dataclass
 class Event(BaseDataclass):
     
     id: int
+    creator: int
     name: str
     description: str
     image_name: str
@@ -16,9 +17,26 @@ class Event(BaseDataclass):
     subroom_id: int
     state: int
     accessibility: int
-    responses: dict = field(default=None)
+    responses: list = field(default=None)
+    images: list = field(default=None)
 
 
     @classmethod
     def from_data(cls, data, **kwargs):
-        return cls()
+        if isinstance(data, list): return [*map(Event.from_data, data)]
+        start_time =  date_to_unix(data["StartTime"])
+        end_time =  date_to_unix(data["EndTime"])
+        return cls(
+            id = data["PlayerEventId"],
+            creator = data["CreatorPlayerId"],
+            name = data["Name"],
+            description = data["Description"],
+            image_name = data["ImageName"],
+            start_time = start_time,
+            end_time = end_time,
+            attendee_count = data["AttendeeCount"],
+            room = data["RoomId"],
+            subroom_id = data["SubRoomId"],
+            state = data["State"],
+            accessibility = data["Accessibility"]
+        )
--- rec_net/managers/event/event_manager.py
+++ rec_net/managers/event/event_manager.py
@@ -1,33 +1,51 @@
 from .event import Event
+from .event_response import EventResponse
 from ..base import BaseManager
 
 class EventManager(BaseManager):
     def __init__(self, client):
-        return super().__init__(client, Event)
+        super().__init__(client, Event)
+        self.configurables = {
+            "responses": self.get_responses,
+            "images": self.get_images,
+            "respondents": self.resolve_respondents,
+            "room": self.resolve_room,
+            "creator": self.resolve_creator
+        }
 
-    async def build_dataclass(self, config, **options):
-        (config
-            .include_responses(options.get("include_responses", False), **options.get("responses_options", {}))
-            .include_images(options.get("include_images", False), **options.get("images_options", {}))
-            .resolve_room(options.get("resolve_room", False), **options.get("room_options", {}))
-            .resolve_creator(options.get("resolve_creator", False), **options.get("creator_options", {}))
-        )
-        return await config.build()
+    @BaseManager.data_method
+    async def get_data(self, id, type):
+        return {
+            "bulk": self.rec_net.api.playerevents.v1.bulk.post(),
+            "id": self.rec_net.api.playerevents.v1(id).get()
+        }
 
-    async def get_data(self, event, type):
-        pass
-
+    @BaseManager.get_method("responses")
     async def get_responses(self, event, **options):
-        pass
+        resp = await self.rec_net.api.playerevents.v1(id).get().fetch()
+        responses = EventResponse.from_data(resp.data)
+        return responses
 
-    async def get_images(self, event, **options):
-        pass
+    @BaseManager.get_method("images", "image")
+    async def get_images(self, id, **options):
+        params = {}
+        params["take"] = options.pop("take", 64)
+        params["skip"] = options.pop("skip", 0)
+        params["sort"] = options.pop("sort", 0)
+        resp = await self.rec_net.api.images.v1.playerevent(id).get(params=params).fetch()
+        return resp.data
+        
 
     async def resolve_respondents(self, event, **options):
         pass
 
+    @BaseManager.resolve_method("room", "room")
     async def resolve_room(self, event, **options):
-        pass
+        if isinstance(event.room, int): return event.room
+        return None
+        
 
+    @BaseManager.resolve_method("creator", "account")
     async def resolve_creator(self, event, **options):
-        pass
+        if isinstance(event.creator, int): return event.creator
+        return None
