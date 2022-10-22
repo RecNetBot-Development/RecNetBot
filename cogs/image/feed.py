a093f9e9ee65fbcb2e80fb6ee4eb27f8436a192d
Jegarde <jesse.tapani.nieminen@gmail.com>
2022-10-22 19:02:43 +0300

    Add feed command

---
+++ cogs/image/feed.py
@@ -0,0 +1,36 @@
+import discord
+from discord.commands import slash_command, Option
+from embeds import fetch_image_embed
+from utils.converters import FetchAccount
+from exceptions import ConnectionNotFound, ImagesNotFound
+from utils.paginator import RNBPaginator, RNBPage
+
+@slash_command(
+    name="feed",
+    description="Browse through a user's RecNet feed."
+)
+async def feed(
+    self, 
+    ctx: discord.ApplicationContext,
+    account: Option(FetchAccount, name="username", description="Enter RR username", default=None, required=False)
+):
+    await ctx.interaction.response.defer()
+
+    if not account:  # Check for a linked RR account
+        account = await self.bot.cm.get_linked_account(self.bot.RecNet, ctx.author.id)
+        if not account: raise ConnectionNotFound
+    
+    posts = await account.get_feed(take=1_000_000)
+    if not posts: raise ImagesNotFound
+        
+    pages = list(map(lambda ele: RNBPage(ele), posts))
+    paginator = RNBPaginator(pages=pages, trigger_on_display=True, show_indicator=False, author_check=True)
+
+    await paginator.respond(ctx.interaction)
+
+    
+    
+
+        
+
+        
