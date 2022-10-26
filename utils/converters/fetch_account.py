7b38372af347e0efaad67ab32caac35da328b594
Jegarde <jesse.tapani.nieminen@gmail.com>
2022-10-26 23:24:37 +0300

    Ignore account in converter

--- utils/converters/fetch_account.py
+++ utils/converters/fetch_account.py
@@ -1,13 +1,17 @@
 import discord
 from discord.ext import commands
 from exceptions import AccountNotFound
+from recnetpy.dataclasses.account import Account
 
 class FetchAccount(commands.Converter):
     """
     Converts a username param to a RR account
     """
-    async def convert(self, ctx: discord.ApplicationContext, username: str):
-        account = await ctx.bot.RecNet.accounts.get(username)
+    async def convert(self, ctx: discord.ApplicationContext, account: str | Account):
+        if isinstance(account, Account):
+            return account
+        
+        account = await ctx.bot.RecNet.accounts.get(account)
         if not account: raise AccountNotFound
         return account
     
