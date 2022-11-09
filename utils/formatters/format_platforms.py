f6e244353838b5a886a8bcf5ab32550e4465bb71
Jegarde <jesse.tapani.nieminen@gmail.com>
2022-11-09 20:26:39 +0200

    Fix PlayStation link

--- utils/formatters/format_platforms.py
+++ utils/formatters/format_platforms.py
@@ -4,7 +4,7 @@ from resources import get_emoji
 PLATFORM_URLS = {
     'steam': "https://store.steampowered.com/app/471710/Rec_Room/", 
     'meta': "https://www.oculus.com/experiences/quest/2173678582678296/", 
-    'playstation': "https://store.playstation.com/en-us/product/EP2526-CUSA09539_00-RECROOM000000001", 
+    'playstation': "https://store.playstation.com/en-us/product/UP2662-CUSA08481_00-RECROOM000000001", 
     'xbox': "https://www.xbox.com/en-ZA/games/store/rec-room/9pgpqk0xthrz",
     'ios': "https://apps.apple.com/us/app/rec-room/id1450306065", 
     'android': "https://play.google.com/store/apps/details?id=com.AgainstGravity.RecRoom",
