4282d8155c64a7e3ed9be2060c931e3bca7bee2a
Jesse Nieminen <jesse.tapani.nieminen@gmail.com>
2022-11-09 20:26:03 +0200

    Fix PlayStation link

--- utility/account/create_platform_section.py
+++ utility/account/create_platform_section.py
@@ -5,7 +5,7 @@ def create_platforms_section(platform_mask: int = None, platform_names: list = N
     platform_info = {
         'Steam': f"{get_emoji('steam')} [`Steam`](https://store.steampowered.com/app/471710/Rec_Room/)", 
         'Meta': f"{get_emoji('oculus')} [`Meta`](https://www.oculus.com/experiences/quest/2173678582678296/)", 
-        'PlayStation': f"{get_emoji('playstation')} [`PlayStation`](https://store.playstation.com/en-us/product/EP2526-CUSA09539_00-RECROOM000000001)", 
+        'PlayStation': f"{get_emoji('playstation')} [`PlayStation`](https://store.playstation.com/en-us/product/UP2662-CUSA08481_00-RECROOM000000001)", 
         'Xbox': f"{get_emoji('xbox')} [`Xbox`](https://www.xbox.com/en-ZA/games/store/rec-room/9pgpqk0xthrz)",
         'iOS': f"{get_emoji('ios')} [`iOS`](https://apps.apple.com/us/app/rec-room/id1450306065)", 
         'Android': f"{get_emoji('android')} [`Android`](https://play.google.com/store/apps/details?id=com.AgainstGravity.RecRoom)",
