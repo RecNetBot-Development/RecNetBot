bd53abc93dad553a4be176bd1eb54fcc6af74291
Jegarde <jesse.tapani.nieminen@gmail.com>
2022-11-29 23:04:48 +0200

    Automatically add user when linked, upgrade embed

--- rr_bot/client.py
+++ rr_bot/client.py
@@ -1,8 +1,22 @@
 import json
 import urllib.parse
+import re
+import itertools
+from utils import date_to_unix
+from datetime import datetime
 from recnetlogin import RecNetLoginAsync
+from recnetpy.dataclasses.account import Account
 from discord.ext import tasks
 from typing import List, Optional
+
+DEVICE_CLASS = [
+    "Unknown",
+    "VR",
+    "Screen Mode",
+    "Mobile",
+    "Quest 1",
+    "Quest 2"
+]
 
 class Client():
     def __init__(self, bot, username: str, password: str):
@@ -14,8 +28,8 @@ class Client():
         self.password = password
         self.login = None  # Filled in start()
         
-        # temporary
-        self.account_id = 81111229
+        # Bot RR account
+        self.account: Account = None
         
         
     async def start(self) -> bool:
@@ -25,8 +39,15 @@ class Client():
             bool: Success
         """
         if self.username and self.password:
+            # Verify credentials
             self.login = RecNetLoginAsync(username=self.username, password=self.password)
             if not await self.login.get_token(): return False
+            
+            # Fetch the RR account
+            self.account = await self.bot.RecNet.accounts.get(self.username)
+            if not self.account: return False
+            
+            # Start
             self.update.start()
             return True
         return False
@@ -93,7 +114,7 @@ class Client():
         return resp.data
     
     
-    async def get_unread_messages(self, thread_id: int, latest_thread_msg_id: int) -> list:
+    async def get_unread_messages(self, thread_id: int, latest_thread_msg_id: int, contents_only: bool = False, max_count: int = 8) -> list:
         """Fetches all the unread messages
 
         Returns:
@@ -101,14 +122,20 @@ class Client():
         """
         
         # Fetch messages
-        resp = await self.bot.RecNet.rec_net.custom("https://chat.rec.net/").thread(thread_id).message.make_request("get", headers=await self.get_headers(), params={"MessageCount": 5, "Mode": 0})
+        resp = await self.bot.RecNet.rec_net.custom("https://chat.rec.net/").thread(thread_id).message.make_request("get", headers=await self.get_headers(), params={"MessageCount": max_count, "Mode": 0})
         if not resp.success: return []
         
         # Get unread messages
-        unread_messages = list(filter(lambda m: m["chatMessageId"] > latest_thread_msg_id and int(m["senderPlayerId"]) != int(self.account_id), resp.data))
+        unread_messages = list(filter(lambda m: m["chatMessageId"] > latest_thread_msg_id and int(m["senderPlayerId"]) != self.account.id, resp.data))
         
         # Return as oldest first
-        return unread_messages.reverse()
+        unread_messages.reverse()
+
+        # If only the contents are wanted
+        if contents_only:
+            unread_messages = list(map(lambda m: self.parse_raw_message(m["contents"]), unread_messages))
+            
+        return unread_messages
 
     
     async def mark_as_read(self, thread_id: int, message_id: int) -> bool:
@@ -163,21 +190,25 @@ class Client():
         return result
     
     
-    async def send_thread_msg(self, text: str, thread_id: int) -> Optional[dict]:
+    async def send_thread_msg(self, messages: List[str], thread_id: int) -> Optional[dict]:
         """Sends a thread message with RecNet's chat API
 
         Args:
-            text (str): Message content
+            messages (List[str]) Messages to send
             thread_id (int): Thread ID to send to
 
         Returns:
             Optional[dict]: Message if successful
         """
         
+        # Safety
+        if not messages: return None
+        
         # Generate the payload
-        message = self.generate_message(text)
-        payload = f"msg={message}"
-
+        payload = ""
+        for text in messages:
+            payload += f"msg={self.generate_message(text)}&"
+            
         # Get headers
         headers = await self.get_headers()
         
@@ -220,20 +251,132 @@ class Client():
         # Accept friend requests
         accepted_ids = []
         for notif in resp.data["Results"]:
-            # Check if it's a friend request
-            if notif["Type"] != 4: continue
-            
             # Get the player's ID
             account_id = notif["FromPlayerId"]
             
-            # Accept request
-            resp = await self.bot.RecNet.rec_net.api.relationships.v3(account_id).make_request("post", headers=headers)
-            if resp.success: accepted_ids.append(account_id)
-            
+            # Check if someone else accepted friend request
+            if notif["Type"] == 40:
+                accepted_ids.append(account_id)
+                
+            # Check if it's a friend request
+            elif notif["Type"] == 4:
+                # Check if it's a linked account
+                method = "post" if self.bot.cm.get_rec_room_connection(rr_id=account_id) else "delete"
+                
+                # Accept or reject request
+                resp = await self.bot.RecNet.rec_net.api.relationships.v3(account_id).make_request(method, headers=headers)
+                if resp.success and method == "post": accepted_ids.append(account_id)
+                
+            # If it's some other notification
+            else:
+                continue
+                
             # Delete the notification
             await self.bot.RecNet.rec_net.api.messages.v3(notif["Id"]).make_request("delete", headers=headers)       
         
         return accepted_ids
+    
+    
+    async def send_friend_request(self, account_id: int) -> bool:
+        """Sends a friend request
+
+        Args:
+            account_id (int): Who to friend
+
+        Returns:
+            bool: Success
+        """
+        resp = await self.bot.RecNet.rec_net.api.relationships.v3(account_id).make_request("post", headers=await self.get_headers())
+        return resp.success
+    
+    
+    def parse_raw_message(self, message: str) -> dict:
+        """Parses a raw stringified message JSON to a dictionary
+
+        Args:
+            message (str): Raw message
+
+        Returns:
+            dict: Parsed dictionary
+        """
+        return json.loads(message)
+    
+    
+    def parse_embeds(self, text: str) -> dict:
+        """Parses room and account embeds from messages
+
+        Args:
+            text (str): Message text
+
+        Returns:
+            dict: Found embed ids
+        """
+        embeds = {}
+        
+        # Find possible account ids
+        account_ids = re.findall("<\@U(\d*)\|", text)
+        if account_ids:
+            embeds["account"] = set(filter(None, account_ids))
+            
+        # Find possible room ids
+        room_ids = re.findall("<\^R(\d*)\|", text)
+        if room_ids:
+            embeds["room"] = set(filter(None, room_ids))
+        
+        return embeds
+    
+    
+    def create_account_embed(self, account_id: int, username: str) -> str:
+        """Generates an account embed for RR chats
+
+        Args:
+            account_id (int): An account's id
+            username (str): The account's username
+
+        Returns:
+            str: RR account chat embed
+        """
+        return f"<@U{account_id}|{username}>"
+    
+    
+    def create_account_details(self, account: Account, matchmaking: dict = None) -> str:
+        """Takes an account dataclass and turns it into a short description about the account.
+
+        Args:
+            account (Account): RecNetPy account
+            matchmaking (dict) Optional matchmaking data for more info
+
+        Returns:
+            str: Accounts details
+        """
+        creation_date = datetime.utcfromtimestamp(account.created_at).strftime('%d %B, %Y')
+        embed = self.create_account_embed(account.id, account.username)
+        junior_status = "🍼" if account.is_junior else "🔞"
+        
+        # Start with account name and junior status
+        fields = [
+            f"{embed} • {account.display_name} {junior_status}",
+        ]
+        
+        # Include platforms if any
+        if account.platforms:
+            fields.insert(1, ", ".join(account.platforms))
+        
+        if matchmaking:
+            # Add latest platform
+            if matchmaking['deviceClass'] > 0 and matchmaking['deviceClass'] < len(DEVICE_CLASS):
+                fields.insert(-1, f"Last played on {DEVICE_CLASS[matchmaking['deviceClass']]}!")
+            
+            # Add last online date if visibility allows
+            if matchmaking["statusVisibility"] == 0 and not matchmaking["isOnline"] and matchmaking["lastOnline"]:
+                last_online_timestamp = date_to_unix(matchmaking["lastOnline"])
+                last_online_date = datetime.utcfromtimestamp(last_online_timestamp).strftime('%d %B, %Y %I %p %Z')
+                fields.insert(-1, f"Last seen at {last_online_date}")
+            
+        # Add creation date last
+        fields.append(f"Created at {creation_date}")
+        
+        return "\n".join(fields)
     
     
     @tasks.loop(seconds=5)
@@ -243,7 +386,8 @@ class Client():
         
         # Send an introduction to just added people
         for account_id in ids:
-            await self.send_dm("hey!", account_id)
+            await self.send_dm("Hey! You can send me @usernames for details.", account_id)
+            print(f"Added id:{account_id}")
             
         # Leave inactive threads
         await self.leave_inactive_threads()
@@ -251,9 +395,37 @@ class Client():
         # Respond to messages
         unread = await self.get_threads(unread_only=True)
         for thread in unread:
-            messages = await self.get_unread_messages(thread["chatThreadId"], thread["lastReadMessageId"])
-            for msg in messages:
-                msg = await self.send_thread_msg(msg["chatMessageId"], msg["chatThreadId"])
+            # Get unread messages
+            messages = await self.get_unread_messages(thread["chatThreadId"], thread["lastReadMessageId"], contents_only=True)
+            if not messages: continue
+            
+            # Get any embeds
+            embeds = list(map(lambda m: self.parse_embeds(m["Data"]), messages))
+            
+            # Build the response
+            response, accounts, rooms, matchmaking = [], [], [], []
+            for e in embeds:
+                for em, ids in e.items():
+                    match em:
+                        case "room":  # if it's a room embed
+                            rooms = await self.bot.RecNet.rooms.fetch_many(ids)
+                        case "account":  # if it's an account embed
+                            accounts = await self.bot.RecNet.accounts.fetch_many(ids)
+                            resp = await self.bot.RecNet.rec_net.custom("https://match.rec.net/player").make_request("post", body={"id": ids}, headers=await self.get_headers())
+                            matchmaking = resp.data
+                        case _:
+                            continue
+            
+            # Build account details
+            for (a, m) in zip(accounts, matchmaking):
+                response.append(self.create_account_details(a, m))
+                
+            if response:
+                await self.send_thread_msg(response, thread["chatThreadId"])
+                print("SENT", response)
+            else:
+                await self.send_thread_msg(["No player embeds found!"], thread["chatThreadId"])
+                print("SENT No player embeds found!")
         
         
     @update.before_loop
