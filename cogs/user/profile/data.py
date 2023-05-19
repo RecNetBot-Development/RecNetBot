4452a8a7106a1edce842fc8b10497fbaf01dba6b
Jegarde <jesse.tapani.nieminen@gmail.com>
2023-05-19 12:53:22 +0300

    Profile data IDS

--- cogs/user/profile/data.py
+++ cogs/user/profile/data.py
@@ -2,7 +2,7 @@ import discord
 from discord.commands import slash_command, Option
 from utils.converters import FetchAccount
 from utils import format_json_block
-from exceptions import ConnectionNotFound
+from exceptions import ConnectionNotFound, AccountNotFound
 
 @slash_command(
     name="data",
@@ -11,9 +11,16 @@ from exceptions import ConnectionNotFound
 async def data(
     self, 
     ctx: discord.ApplicationContext, 
-    account: Option(FetchAccount, name="username", description="Enter RR username", default=None, required=False)
+    account: Option(FetchAccount, name="username", description="Enter RR username", default=None, required=False),
+    _id: Option(int, name="id", description="Enter RR account ID instead", default=None, required=False)
 ):
     await ctx.interaction.response.defer()
+
+    # Prioritize IDs
+    if _id:
+        account = await ctx.bot.RecNet.accounts.fetch(_id)
+        if not account: raise AccountNotFound
+        return await ctx.respond(content=format_json_block(account.data))
 
     if not account:  # Check for a linked RR account
         account = await self.bot.cm.get_linked_account(self.bot.RecNet, ctx.author.id)
