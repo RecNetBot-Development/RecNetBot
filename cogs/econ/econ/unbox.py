d134ad3178e6bb3a147f8978bf9958d12fa1b50c
Jegarde <jesse.tapani.nieminen@gmail.com>
2023-06-08 12:51:27 +0300

    Unbox shortcuts

--- cogs/econ/econ/unbox.py
+++ cogs/econ/econ/unbox.py
@@ -16,7 +16,7 @@ class BoxTypes(Enum):
 class BoxOption(discord.ui.Button):
     def __init__(self, option: int):
         self.option = option
-        item = ITEMS[self.option]
+        item = get_item(item_id=self.option)
         name = item["name"]
  
         super().__init__(style=discord.ButtonStyle.primary, label=name, custom_id=str(self.option))
@@ -43,12 +43,50 @@ class BoxAgain(discord.ui.Button):
         await view.respond(ctx)
 
 
+class InventoryButton(discord.ui.Button):
+    def __init__(self, inv_cmd):
+        super().__init__(style=discord.ButtonStyle.gray, label="Inventory")
+        self.inv_cmd = inv_cmd
+
+    async def callback(self, interaction: discord.Interaction):
+        self.disabled = True
+        await interaction.message.edit(view=self.view)
+
+        # Run the inv command
+        ctx = await self.view.bot.get_application_context(interaction)
+        await self.inv_cmd(ctx)
+
+
+class SellButton(discord.ui.Button):
+    def __init__(self, item: dict, ecm):
+        super().__init__(style=discord.ButtonStyle.green)
+        self.ecm = ecm
+        self.item = item
+
+        # Button label
+        self.label = f"Sell for {self.item['tokens']}"
+
+    async def callback(self, interaction: discord.Interaction):
+        self.disabled = True
+        await interaction.message.edit(view=self.view)
+
+        # Remove item from inv
+        self.ecm.add_item(interaction.user.id, self.item['id'], -1)
+        # Add tokens
+        self.ecm.add_tokens(interaction.user.id, self.item['tokens'])
+
+        await interaction.response.send_message(f"Sold {self.item['emoji_icon']}** {self.item['name']}** for {get_emoji('token')}**{self.item['tokens']}**!")
+
+
 class BoxMenu(discord.ui.View):
-    def __init__(self, bot, box_type: BoxTypes):
+    def __init__(self, bot, box_type: BoxTypes, inv_cmd = None):
         super().__init__(
             timeout=600,
             disable_on_timeout=True
         )
+
+        # Inventory command
+        self.inv_cmd = inv_cmd
 
         # Rewards
         self.box_type = box_type
@@ -76,12 +114,20 @@ class BoxMenu(discord.ui.View):
         # Clear reward options
         self.clear_items()
 
-        # Again button
-        self.add_item(BoxAgain(self.box_type))
-
         # Get the chosen reward
         reward = get_item(item_id=item_id)
 
+        # Again button
+        self.add_item(BoxAgain(self.box_type))
+
+        # Sell button
+        self.add_item(SellButton(reward, self.bot.ecm))
+
+        # Inventory button
+        if self.inv_cmd:
+            self.add_item(InventoryButton(self.inv_cmd))
+
+        # Reward embed
         em = get_default_embed()
         em.title = "Reward chosen!"
         em.description = f"**{reward['name']}** (x{amount})" \
@@ -125,7 +171,7 @@ class BoxMenu(discord.ui.View):
         em.set_footer(text=f"{self.ctx.author.name}'s box")
 
         for i in self.items:
-            em.add_field(name=f"{i['emoji_icon']} {i['name']}", value=f"{get_emoji('token')} {i['tokens']:,}")
+            em.add_field(name=f"{i['emoji_icon']} {i['name']}", value=f"{get_emoji('token')} {i['tokens']:,} {get_emoji('level') * i['rarity']}")
 
         return em
 
@@ -161,7 +207,11 @@ async def unbox(
 ):
     box_type = BoxTypes.LOW_TIER if box == "1-2 Stars (Free)" else BoxTypes.HIGH_TIER
 
-    view = BoxMenu(self.bot, box_type)
+    # Get inventory command
+    group = discord.utils.get(self.__cog_commands__, name='econ')
+    inv_cmd = discord.utils.get(group.walk_commands(), name='inventory')
+
+    view = BoxMenu(self.bot, box_type, inv_cmd)
     await view.respond(ctx)
 
     
