1bc7d6bb6b69fcb08becee2f4a80ce1d007b161c
Jegarde <jesse.tapani.nieminen@gmail.com>
2023-06-19 12:10:39 +0300

    Make search room default to owned rooms

--- utils/autocompleters/search_room.py
+++ utils/autocompleters/search_room.py
@@ -1,5 +1,6 @@
 import discord
 from recnetpy.dataclasses.room import Room
+from recnetpy.dataclasses.account import Account
 from recnetpy.rest.exceptions import BadRequest
 from typing import List
     
@@ -8,10 +9,21 @@ async def room_searcher(ctx: discord.AutocompleteContext) -> List[str]:
     Returns a list of matching RR rooms
     """
 
+    rooms: List[Room] = []
+
     try:
-        rooms: List[Room] = await ctx.bot.RecNet.rooms.search(query=ctx.value)
+        rooms = await ctx.bot.RecNet.rooms.search(query=ctx.value)
     except BadRequest:
-        rooms: List[Room] = await ctx.bot.RecNet.rooms.hot(take=20)
+        # Check if the user is linked
+        check_discord = ctx.bot.cm.get_discord_connection(ctx.interaction.user.id)
+        if check_discord:
+            # If they are, show their created rooms
+            user: Account = await ctx.bot.RecNet.accounts.fetch(check_discord.rr_id)
+            rooms = await user.get_owned_rooms()
+
+    # If no rooms, just show hot ones
+    if not rooms:
+        rooms = await ctx.bot.RecNet.rooms.hot(take=20)
 
     # Return results
     return [room.name for room in rooms]
