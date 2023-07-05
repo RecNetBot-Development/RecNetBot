389c7a1f0cfe578ea299dcc3a16f2af10f84c01b
Jegarde <jesse.tapani.nieminen@gmail.com>
2023-07-05 13:08:17 +0300

    Make JSON blocks valid

--- utils/format_json_block.py
+++ utils/format_json_block.py
@@ -5,5 +5,5 @@ def format_json_block(raw_json: dict) -> str:
     Formats raw json data into a json block that is sent to Discord
     """
     
-    formatted = json.dumps(raw_json, indent=4, separators=(",", " = "))
+    formatted = json.dumps(raw_json, indent=4, separators=(",", ": "))
     return f"```json\n{formatted}```"
