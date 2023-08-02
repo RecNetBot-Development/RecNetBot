956a443058f3556e13739022e9faf631a9b5bd6d
Jegarde <jesse.tapani.nieminen@gmail.com>
2023-08-02 16:17:25 +0300

    Support pinging linked users in username slots

--- utils/converters/fetch_account.py
+++ utils/converters/fetch_account.py
@@ -11,14 +11,22 @@ class FetchAccount(commands.Converter):
         if isinstance(_account, Account):
             return _account
         
-        # Sanitize input
-        account_name = _account.strip().replace("@", "")
-        
-        if account_name and account_name.isascii():
-            account = await ctx.bot.RecNet.accounts.get(account_name)
+        # Check if Discord mention
+        if _account.startswith("<@") and _account.endswith(">"):
+            # This should be a Discord mention!
+            discord_id = _account.split("<@")[1].split(">")[0]
+            linked_account = await ctx.bot.cm.get_linked_account(ctx.bot.RecNet, discord_id)
+            if not linked_account: raise AccountNotFound(_account)
+            return linked_account
         else:
-            account = None
+            # Sanitize input
+            account_name = _account.strip().replace("@", "")
+            
+            if account_name and account_name.isascii():
+                account = await ctx.bot.RecNet.accounts.get(account_name)
+            else:
+                account = None
 
-        if not account: raise AccountNotFound(_account)
-        return account
+            if not account: raise AccountNotFound(_account)
+            return account
     
