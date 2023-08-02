0ff55658a5804681091d8a6d263d54179246de47
Jegarde <jesse.tapani.nieminen@gmail.com>
2023-08-02 16:15:55 +0300

    Option to check other discord user's linked acc

--- cogs/user/profile/linked.py
+++ cogs/user/profile/linked.py
@@ -10,16 +10,20 @@ from datetime import datetime, timedelta
 # User can only run the command once every "per" seconds
 @slash_command(
     name="linked",
-    description="Check what Rec Room account is linked to your Discord."
+    description="Check which Rec Room account is linked to a Discord account."
 )
 async def linked(
     self, 
-    ctx: ApplicationContext
+    ctx: ApplicationContext,
+    mention: Option(discord.User, description="Whose Discord account should be checked?", required=False)
 ):
     await ctx.interaction.response.defer(ephemeral=True)
     
+    # Get the wanted Discord user
+    discord_user = mention if mention else ctx.author
+
     # Check if Discord user has already linked an account
-    check_discord = self.bot.cm.get_discord_connection(ctx.author.id)
+    check_discord = self.bot.cm.get_discord_connection(discord_user.id)
     if check_discord:
         user = await self.bot.RecNet.accounts.fetch(check_discord.rr_id)
         
@@ -27,8 +31,12 @@ async def linked(
         em = get_default_embed()
         group = discord.utils.get(self.__cog_commands__, name='profile')
         unlink_command = discord.utils.get(group.walk_commands(), name='unlink')
-        em.description = f"You are linked to [@{user.username}]({profile_url(user.username)})."
-        
+
+        if discord_user == ctx.author:
+            em.description = f"Your Discord account linked to [@{user.username}]({profile_url(user.username)})."
+        else:
+            em.description = f"{discord_user.mention} is linked to [@{user.username}]({profile_url(user.username)})."
+                
         return await ctx.interaction.edit_original_response(
             embeds=[profile_em, em]
         )
@@ -36,6 +44,12 @@ async def linked(
     else:
         group = discord.utils.get(self.__cog_commands__, name='profile')
         unlink_command = discord.utils.get(group.walk_commands(), name='link')
-        await ctx.interaction.edit_original_response(
-            content=f"Your Discord account has no linked Rec Room account! Feel free to link one with {unlink_command.mention}"
-        )     
+
+        if discord_user == ctx.author:
+            await ctx.interaction.edit_original_response(
+                content=f"Your Discord account has not been linked to a Rec Room account! Feel free to link one with {unlink_command.mention}"
+            )     
+        else:
+            await ctx.interaction.edit_original_response(
+                content=f"{discord_user.mention} has not been linked to a Rec Room account."
+            )  
